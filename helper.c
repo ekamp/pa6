34861cc3b476eaff81653d095815d50747eceff8
ekamp <ekamp@eden.rutgers.edu>
2012-12-05 13:19:32 -0500

    Adding things

--- helper.c
+++ helper.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+#include<stdlib.h>
 
 struct User * openDatabase (FILE * dbFile)
 {
@@ -14,7 +16,7 @@ struct User * openDatabase (FILE * dbFile)
 	char * uid;
 	char * credits;
 	
-	if(dbFile == NULL)
+	if (dbFile == NULL)
 	{
 		return NULL;
 	}
@@ -69,6 +71,8 @@ void openOrders (FILE * bookOrderFile,struct User *users)
 	char * bookTitle;
 	char * cost;
 	char * uid;
+    struct Node * temp;
+    struct User * currentUser;
 	if(bookOrderFile == NULL || users == NULL)
 	{
 		return;
@@ -79,44 +83,68 @@ void openOrders (FILE * bookOrderFile,struct User *users)
 		strcpy(cost,strtok(NULL,"|"));
 		strcpy(uid,strtok(NULL,"|"));
 		
-		//newUser = createUser(username,uid,atof(credits));
+        currentUser = (struct User)users[atoi(uid)];
+	    purchase(currentUser->success, currentUser->fail,currentUser,cost,bookTitle);
 	}
-	
-	
+    fclose(bookOrderFile);
 }
 
 
-void printToFile (Node[] userArray)
+void printToFile (Node* userArray)
 {
 	int i = 0;
+    int length;
 	if (userArray == NULL)
 	{
 		return;
 	}
 	else
 	{
-		while(userArray[i] != NULL)
+        length = sizeof(userArray)/sizeof(struct Node);
+		while(i<length)
 		{
 			printf("%s",(char *)userArray[i]->data->username);
-			while(userArray[i]
 			printf("=== BEGIN CUSTOMER INFO ===\n");
-			
+			printf("### BALANCE ###\n");
+            printf("Customer name: %f",userArray[i]->data->username);
+            printf("Customer ID number: %f",userArray[i]->data->uid);
+            printf("Remaining credit balance after all purchases (a dollar amount): %d",userArray[i]->data->remainingCredits);
+            i++;
 		}
 		
 	}
 }
 
 
-struct User createUser(char *username,int uid,double initialCredits)
+void purchase(struct Node * success, struct Node * fail,struct User * currentUser,double cost,char *bookTitle)
 {
-	struct User newUser = calloc(1,sizeof(struct User));
-	newUser->username = username;
-	newUser->uid = uid;
-	newUser->initialCredits = initialCredits;
-	pthread_mutex_init(newUser->userMutex,NULL);
+   struct Node *temp;
+   struct order *orderTemp;
+   if (currentUser->remainingCredits - cost < 0)
+   {
+        /*Add to the fail node list */
+        temp = calloc(1,sizeof(struct Node));
+        order = calloc(1,sizeof(struct order));
+        order->bookTitle = bookTitle;
+        order->cost = cost;
+        order->currentCredits = currentUser->remainingCredits;
+        temp->data = order;
+        fail->next = temp;
+   }
+   else
+   {
+        /* Add to the success node list */
+        temp = calloc(1,sizeof(struct Node));
+        order = calloc(1,sizeof(struct order));
+        order->bookTitle = bookTitle;
+        order->cost = cost;
+        order->currentCredits = currentUser->remainingCredits-cost;
+        temp->data = order;
+        success->next = temp;
+   }
 }
 
-struct Order createOrder(char *bookTitle,double cost, int uid,)
+struct User createUser(char *username,int uid,double initialCredits)
 {
 	struct User newUser = calloc(1,sizeof(struct User));
 	newUser->username = username;
